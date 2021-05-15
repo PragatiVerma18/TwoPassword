d72058232671b6e1665c2b5f56b55b05de4bf676
PragatiVerma18 <itispragativerma@gmail.com>
2021-05-15 17:32:18 +0530

    Added session auth

--- twopassword/settings.py
+++ twopassword/settings.py
@@ -81,10 +81,11 @@ AUTHENTICATION_BACKENDS = (
 )
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
